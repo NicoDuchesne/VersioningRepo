cb71563fb75308b51940af9a133849998fb8f857
Mathieu Guitter <guittermathieu@gmail.com>
2024-10-16 15:01:36 +0200

    [CHANGE] EnemyTakeDamage
    add Knockback when hit

--- Assets/Scripts/EnemyTakeDamage.cs
+++ Assets/Scripts/EnemyTakeDamage.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class EnemyTakeDamage : MonoBehaviour
 {
-    private int health = 1;
+    [SerializeField] private int health;
     private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite spriteDeath;
+    private CircleCollider2D colliderEnemy;
+    private Rigidbody2D rb;
+    [SerializeField] private float forceKnockback;
+    [SerializeField] private float forceKnockbackTime;
+    [SerializeField] private Transform playerTop;
+    private float distanceX;
+    private float distanceY;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        colliderEnemy = GetComponent<CircleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void TakeDamage(int damage)
@@ -17,7 +27,19 @@ public class EnemyTakeDamage : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            spriteRenderer.color = Color.yellow;
+            spriteRenderer.sprite = spriteDeath;
+
+            colliderEnemy.enabled = false;
         }
+        distanceX = playerTop.position.x - transform.position.x;
+        distanceY = playerTop.position.y - transform.position.y;
+        rb.AddForce(new Vector2(-distanceX * forceKnockback, -distanceY * forceKnockback), ForceMode2D.Impulse);
+        StartCoroutine(KnockbackTime(rb));
+    }
+
+    private IEnumerator KnockbackTime(Rigidbody2D enemyRb)
+    {
+        yield return new WaitForSeconds(forceKnockbackTime);
+        enemyRb.velocity = Vector2.zero;
     }
 }
