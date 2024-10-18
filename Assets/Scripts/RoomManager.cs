dfc093de1346a65c9400b8ce581147d44e9b548f
Mathieu Guitter <guittermathieu@gmail.com>
2024-10-18 02:35:16 +0200

    [ADD] RoomManager
    also get more "Salle" to MathieuScene

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    private GameObject[] rooms;
+    [SerializeField] private GameObject RoomContainer;
+    private int previusRandomRoom = 0;
+
+    private void Start()
+    {
+        rooms = new GameObject[RoomContainer.transform.childCount];
+        for (int i = 0; i < RoomContainer.transform.childCount; i++)
+        {
+            rooms[i] = RoomContainer.transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void ChooseRandomRoom()
+    {
+        int randomRoom = Random.Range(0, rooms.Length);
+        rooms[previusRandomRoom].SetActive(false);
+        rooms[randomRoom].SetActive(true);
+        Debug.Log(" Current Room: " + randomRoom);
+        previusRandomRoom = randomRoom;
+    }
+
+}
